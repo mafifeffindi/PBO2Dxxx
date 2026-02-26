11795240bfe9aea140a8b78e7957f156f19700cb
Muhamad Afif Effindi <bungah.techno@gmail.com>
2026-02-26 10:32:56 +0700

    Add files via upload
    Tugas pekan 03

---
+++ Pekan 03/Students.java
@@ -0,0 +1,13 @@
+package oopbasics;
+
+public class Students {
+
+    String name;
+    int age;
+
+    void introduce() {
+        System.out.println(
+            "Hi, my name is " + name + " and I am " + age + " years old."
+        );
+    }
+}
